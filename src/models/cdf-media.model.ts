b71af00e10fab2c7edab04751b1c1bdb6e184f31
Tito Agudelo <titoarturoagudelo@gmail.com>
2017-07-13 22:22:18 -0500

    image home media model

--- src/models/cdf-media.model.ts
+++ src/models/cdf-media.model.ts
@@ -4,18 +4,25 @@ import { CdfVideoModel } 			from './cdf-video.model';
 export class CdfMediaModel extends CdfRootModel
 {
 	ImageUri: string;
+	ImageHomeUri: string;
 	YouTubeId: string;
 	VideoList: CdfVideoModel[] = [];
 	HasImage: boolean = false;
 	HasVideo: boolean = false;
 
-	constructor(id?: string, type?: string, title?:string, description?: string, imageUrl?: string, youTubeId?:string, videoList?: CdfVideoModel[], body?: string)
+	constructor(id?: string, type?: string, title?:string, description?: string, imageUrl?: string, youTubeId?:string, videoList?: CdfVideoModel[], body?: string, imageHomeUrl?: string)
 	{
 		super(id, type, title, description, body);
 
 		if (imageUrl)
 		{
 			this.ImageUri = imageUrl;
+			this.HasImage = true;
+		}
+
+		if (imageHomeUrl)
+		{
+			this.ImageHomeUri = imageHomeUrl;
 			this.HasImage = true;
 		}
 
@@ -35,6 +42,12 @@ export class CdfMediaModel extends CdfRootModel
 	SetImage(uri:string)
 	{
 		this.ImageUri = uri;
+		this.HasImage = true;
+	};
+
+	SetHomeImage(uri:string)
+	{
+		this.ImageHomeUri = uri;
 		this.HasImage = true;
 	};
 
